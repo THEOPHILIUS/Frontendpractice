ce9e866d01681c9e46990ab58eebee5ad2b695d9
THEOPHILIUS <theophiliuskehinde@gmail.com>
2024-01-17 14:13:26 +0100

    feat: Arithmetic

---
+++ math.js
@@ -0,0 +1,9 @@
+const num1 = 10
+const num2 = 3
+const num3 = 7
+
+console.log(addition_result = num1 + num2)
+console.log(subtraction_result = num3 - num2)
+console.log(multiplication_result = num1 * num3)
+console.log(division_result = num3 / num2)
+console.log(modulus_result = num1 % num2)
